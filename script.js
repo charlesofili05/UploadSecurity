04d204390d3bc1507d5206451a3d303dcf45564f
charlesofili05 <166115638+charlesofili05@users.noreply.github.com>
2024-04-06 11:58:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("burger-menu").addEventListener("click", function() {
     this.classList.toggle("closed");
     document.getElementById("mobileNavLinkPage").classList.toggle("open");
+    document.getElementById("mobileNav").classList.toggle("fixednav");
     document.body.classList.toggle("no-scroll");
     
   });
@@ -11,3 +12,22 @@ document.getElementById("burger-menu").addEventListener("click", function() {
     document.body.classList.remove("no-scroll");
     }
   })
+
+  let readMoreBtns = document.querySelectorAll(".readMoreBtn")
+  let readLessBtns = document.querySelectorAll(".readLessBtn")
+
+  readMoreBtns.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+      btn.classList.add("hide");
+      btn.parentNode.childNodes[3].classList.add("show")
+      btn.parentNode.childNodes[4].classList.add("show")
+    })
+  });
+  readLessBtns.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+      btn.classList.remove("show");
+      btn.parentNode.childNodes[3].classList.remove("show")
+      btn.parentNode.childNodes[1].classList.remove("hide")
+    })
+  });
+
